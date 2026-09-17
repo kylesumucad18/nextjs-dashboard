2b28572f617c44e3ac79de5f4d95cb0e4e87df4d
kylehatescodes <your-email-associated-with-kylehatescodes>
2026-09-17 17:39:44 +0800

    pls deploy

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,14 +110,13 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 
 export async function deleteInvoice(id: string) {
     // Throwing an intentional error to test the error.tsx file
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
 
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
-        return { message: 'Deleted Invoice.' };
     } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.' };
+        console.error('Database Error: Failed to Delete Invoice.', error);
     }
 }
 
